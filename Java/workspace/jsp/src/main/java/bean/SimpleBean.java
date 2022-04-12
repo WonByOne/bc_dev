166b9fc3cf55976f5c751711ad5e77a9802a15aa
WonByOne <meekbiker@gmail.com>
2022-04-13 00:06:09 +0900

    Making Bean

---
+++ Java/workspace/jsp/src/main/java/bean/SimpleBean.java
@@ -0,0 +1,35 @@
+package bean;
+
+public class SimpleBean {	// 변수는 tag에 사용할 변수명과 일치하게
+	private String name;
+	private int age;
+	private String tel;
+	private String addr;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	public String getAddr() {
+		return addr;
+	}
+	public void setAddr(String addr) {
+		this.addr = addr;
+	}
+	
+	
+}
