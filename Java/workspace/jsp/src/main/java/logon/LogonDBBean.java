9fa9c52e9f7910f3b93f0cd5aedcc3743e55bd97
WonByOne <meekbiker@gmail.com>
2022-04-15 08:50:48 +0900

    preparedstatement 사용

--- Java/workspace/jsp/src/main/java/logon/LogonDBBean.java
+++ Java/workspace/jsp/src/main/java/logon/LogonDBBean.java
@@ -1,6 +1,8 @@
 package logon;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import javax.naming.Context;
@@ -22,5 +24,70 @@ public class LogonDBBean {
 		return ds.getConnection();
 	}
 	
+	public int insertMember(LogonDataBean dto) {
+		int result = 0; // 작업 실패여부를 알기 위한 변수
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		try {
+			con = getConnection();
+			String sql =
+					"insert into member values(?, ?, ?, ?, ?, ?, ?, ?)";
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, dto.getId()); // DB의 index는 1부터
+			pstmt.setString(2, dto.getPasswd());
+			pstmt.setString(3, dto.getName());
+			pstmt.setString(4, dto.getBirth());
+			pstmt.setString(5, dto.getSsn());
+			pstmt.setString(6, dto.getTel());
+			pstmt.setString(7, dto.getEmail());
+			pstmt.setTimestamp(8, dto.getReg_date());
+			result = pstmt.executeUpdate(); // 몇줄 업데이트 했는지 리턴
+		} catch(NamingException e) {
+			e.printStackTrace();
+		} catch(SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if(con != null) con.close();
+				if(pstmt != null) pstmt.close();
+			} catch(SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return result;
+	}
+	
+	public int checkid(String id) {
+		int result = 0;
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			con = getConnection();
+			String sql = "select * from member where id=?";
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, id);
+			rs = pstmt.executeQuery();
+			if(rs.next()) {	// id 있음
+				result = 1;
+			} else {		// id 없음
+				result = 0;
+			}
+			
+		} catch (NamingException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if(con != null) con.close();
+				if(pstmt != null) pstmt.close();
+				if(rs != null) rs.close(); 
+			} catch(SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return result;
+	}
 	
 } // class
