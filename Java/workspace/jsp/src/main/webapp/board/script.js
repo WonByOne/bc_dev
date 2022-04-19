86f75cc76821db2afe253ea0d0a98e5a016d38a3
WonByOne <meekbiker@gmail.com>
2022-04-20 08:56:25 +0900

    javascript: 글 삭제 시 비밀번호 공백 확인

--- Java/workspace/jsp/src/main/webapp/board/script.js
+++ Java/workspace/jsp/src/main/webapp/board/script.js
@@ -3,6 +3,14 @@ var subjectstr = "제목을 입력하세요.";
 var contentstr = "내용을 입력하세요.";
 var passwdstr = "비밀번호를 입력하세요.";
 
+var passwderror = "Wrong password.\nCheck password.";
+var deleteerror = "Failed to delete.\nTry again after a while.";
+
+function erroralert(msg) {
+	alert(msg);
+	history.back();
+}
+
 // 글 작성
 function writecheck() {
 	if(! writeform.writer.value) {
