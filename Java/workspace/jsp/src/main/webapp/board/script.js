da67295e6c95af7cc0d6423609a4f7130a3697be
WonByOne <meekbiker@gmail.com>
2022-04-22 08:13:10 +0900

    수정할 때의 이벤트 체크 추가

--- Java/workspace/jsp/src/main/webapp/board/script.js
+++ Java/workspace/jsp/src/main/webapp/board/script.js
@@ -5,10 +5,27 @@ var passwdstr = "비밀번호를 입력하세요.";
 
 var passwderror = "Wrong password.\nCheck password.";
 var deleteerror = "Failed to delete.\nTry again after a while.";
+var modifyerror = "Failed to modify.\nTry again after a while.";
 
 function erroralert(msg) {
 	alert(msg);
 	history.back();
+}
+// 글 수정 
+function modifycheck() {
+	if(! modifyform.subject.value) {
+		alert(subjectstr);
+		modifyform.subject.focus();
+		return false;
+	} else if(! modifyform.content.value) {
+		alert(contentstr);
+		modifyform.content.focus();
+		return false;
+	} else if(! modifyform.passwd.value) {
+		alert(passwdstr);
+		modifyform.passwd.focus();
+		return false;
+	}
 }
 
 // 글 작성
