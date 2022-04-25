b54690c77ee45a0e1ac32fab7cec9ad3add0a5da
WonByOne <meekbiker@gmail.com>
2022-04-26 08:31:25 +0900

    EL 태그 예제용 BEAN

---
+++ Java/workspace/jsp/src/main/java/el/User.java
@@ -0,0 +1,33 @@
+package el;
+
+public class User {	// form에 있는 이름과 같게 만들 것
+	private String name;
+	private int age;
+	private String gender;
+	private String[] hobby;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String[] getHobby() {
+		return hobby;
+	}
+	public void setHobby(String[] hobby) {
+		this.hobby = hobby;
+	}
+}
