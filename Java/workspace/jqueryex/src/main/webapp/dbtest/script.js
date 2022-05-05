e8ce44e0003c8e7e79e0aa9e227df8b75eafa90a
WonByOne <meekbiker@gmail.com>
2022-05-06 07:04:45 +0900

    AJAX - DB 유효성검사 script

--- Java/workspace/jqueryex/src/main/webapp/dbtest/script.js
+++ Java/workspace/jqueryex/src/main/webapp/dbtest/script.js
@@ -23,5 +23,40 @@ $(document).ready(
 				);
 			}
 		);
+		// 비밀번호 
+		$("input[name=repasswd]").on(
+			"keyup",
+			function(event) {
+				var passwd = $("input[name=passwd]").val();
+				var repasswd = $("input[name=repasswd]").val();
+				if(passwd == repasswd) {
+					$("#passwdcheck").html("Password OK.");
+				} else {
+					$("#passwdcheck").html("Check password.");
+				}
+			}
+		);
+		// 전화번호
+		$("input[name=tel]").on(
+			"keyup",
+			function(event) {
+				$.ajax(
+					{
+						type : "POST",
+						data : {				//  넘길 데이터 정의 JSON 형식
+							tel : $("input[name=tel]").val()
+						},
+						url : "telcheck.jsp",
+						dataType : "text",
+						success : function(data) {
+							$("#telcheck").html(data);
+						},
+						error : function(error) {
+							$("#telcheck").html(error);
+						}
+					}					
+				);
+			}
+		);
 	}
 );
