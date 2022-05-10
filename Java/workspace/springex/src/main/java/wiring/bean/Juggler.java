a5a8067e416a871965c320f54d50730ec57241c8
WonByOne <meekbiker@gmail.com>
2022-05-10 09:16:09 +0900

    Spring bean wiring 예제 수정

--- Java/workspace/springex/src/main/java/wiring/bean/Juggler.java
+++ Java/workspace/springex/src/main/java/wiring/bean/Juggler.java
@@ -1,8 +1,9 @@
 package wiring.bean;
 
 public class Juggler implements Performer {
-	private int beanBags;
-	public Juggler(int beanBags) {
+	int beanBags = 3;
+	public Juggler() {}	// 디폴트 생성
+	public Juggler(int beanBags) {	// 자식으로부터 넘어오는 beanBags 처
 		this.beanBags = beanBags;
 	}
 	@Override
--- Java/workspace/springex/src/main/java/wiring/bean/Simple.java
+++ Java/workspace/springex/src/main/java/wiring/bean/Simple.java
@@ -1,14 +1,40 @@
 package wiring.bean;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Simple {
 	public static void main(String[] args) throws PerformanceException {
-		Juggler kim = new Juggler(3);	// Strong bind
+		Juggler kim = new Juggler();	// Strong bind
 		kim.perform();
 		
 		// loose bind
-		Performer lee = new Juggler(4);
+		Performer lee = new Juggler();
 		lee.perform();
 		
 		// DI 종속 객체 주입
+		ApplicationContext ctx 
+			= new ClassPathXmlApplicationContext("/wiring/bean/ApplicationContext.xml");
+		Performer hong = (Performer) ctx.getBean("hong");
+		hong.perform();
+		
+		// singleton / prototype
+//		Ticket t1 = new Ticket();  // 서로 다른 객체 (prototype과 같음)
+//		Ticket t2 = new Ticket();
+		Ticket t1 = (Ticket) ctx.getBean("ticket");	// 객체 공유 (singleton)
+		Ticket t2 = (Ticket) ctx.getBean("ticket");  
+		
+		
+		t1.reservation();
+		t2.reservation();
+		
+		// 빈 초기화와 정리 
+		// Auditorium auditorium = (Auditorium) ctx.getBean("auditorium");
+		
+		
+		System.out.println();
+		// 연습
+		Performer singer = (Performer) ctx.getBean("singer");
+		singer.perform();
 	}
 }
